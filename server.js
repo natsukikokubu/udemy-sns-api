28fd9fac38386c022c33316b56c817ffd17e33aa
natsukikokubu <natsukikokubu3@gmail.com>
2024-03-31 19:24:40 +0900

    done

---
+++ server.js
@@ -0,0 +1,23 @@
+const { Prisma } = require("@prisma/client");
+const express = require("express");
+const app = express();
+const authRoute = require("./routers/auth");
+const postsRoute = require("./routers/posts");
+const usersRoute = require("./routers/users");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const PORT = 4050;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
+app.use("/api/posts", postsRoute);
+
+app.use("/api/users", usersRoute);
+
+app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
